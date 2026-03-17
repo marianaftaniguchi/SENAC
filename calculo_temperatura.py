cd8513a5532c6f37ee956a50b0c8e309ceacd77d
Mariana Favero Taniguchi <marianaf.taniguchi@gmail.com>
2026-03-17 18:04:15 -0300

    Add files via upload

---
+++ calculo_temperatura.py
@@ -0,0 +1,5 @@
+temperatura_F=float(input("Insira aqui a temperatura em Fahrenheit : ")) #Saber temperatura em fahrenheit 
+
+temperatura_C= (5 * temperatura_F- 160) / 9 #fórmula para conversão
+
+print(f"O resultado em Celsius é: {temperatura_C:.2f}","C") #resultado
